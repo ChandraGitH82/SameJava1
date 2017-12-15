3e3668ffe79cfe290f5ec5b59ff943f03676d8ad
ChandraGolla <chandra704.golla@gmail.com>
2017-12-15 13:59:42 +0530

    Adding Java files

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+package testPackage;
+
+public class Helloworld {
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+	System.out.println ("Hello World!");
+    }
+}
